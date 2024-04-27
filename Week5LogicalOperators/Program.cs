9e3703a284009e6c680cd1a3e0c7b1e4d3d046f0
RomanEnger <Roman.engertzisikov@gmail.com>
2024-04-27 14:32:52 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+//AND - &&
+
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
+using System.ComponentModel.Design;
+
+string userPIN, userName, password;
+Console.WriteLine("Enter PIN:");
+userPIN = Console.ReadLine();
+
+if(userPIN == "1234")
+{
+    Console.WriteLine("Enter your username:");
+    userName = Console.ReadLine();
+    Console.WriteLine("Enter your password:");
+    password = Console.ReadLine();
+    if(userName == "admin1" && password == "admin1234")
+    {
+        Console.WriteLine("Welcome!");
+    }
+    else
+    {
+        Console.WriteLine("Invalid login credentials.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid PIN.");
+}
